320d3ebf90ee6505a0e6da19de9a2d40f3f1bb8a
= <jackwillee1995@gmail.com>
2022-11-15 22:45:29 +0100

    first user story completed can send and view posts on the homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,29 @@
 class PostsController < ApplicationController
 
     def index
+    @posts = Post.all
+    #to access all posts from the model/database#
 
     end 
+
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+    
+        if @post.save
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+
+    end 
+
+    private
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
